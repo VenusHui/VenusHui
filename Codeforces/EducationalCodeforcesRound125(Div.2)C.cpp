f39ec52c7a7c02f3db719921e4efadf09f7d4910
VenusHui <2913727619@qq.com>
2022-03-27 20:39:53 +0800

    Update EDU-CF#125

--- Codeforces/EducationalCodeforcesRound125(Div.2)C.cpp
+++ Codeforces/EducationalCodeforcesRound125(Div.2)C.cpp
@@ -17,9 +17,7 @@ using namespace std;
 
 // Educational Codeforces Round 125 (Div. 2) C. Bracket Sequence Deletion
 
-int length;
-int n, ans = 0;
-
+// 补题passed
 int main()
 {
     ios_base::sync_with_stdio(false);
@@ -29,104 +27,161 @@ int main()
     cin >> t;
     while (t--)
     {
-        cin >> n;
-        string str;
-        cin >> str;
-        ans = 0, length = n;
-        int ll = 0;
-        while (ll <= n)
-        {
-            int reflag = 0, paflag = 0;
-            // regular
-            stack<char> s;
-            int tmp = 0;
-            for (int i = ll; i < n; i++)
-            {
-                if (str[i] == '(')
-                {
-                    s.push('(');
-                    tmp++;
-                }
-                else
-                {
-                    if (!s.empty())
+        int n;
+        string s;
+        cin >> n >> s;
+        int operNum = 0, pos = 0;
+        while (pos < n) {
+            if (s[pos] == '(' && pos + 1 < n) {
+                operNum++;
+                pos += 2;
+            }
+            else {
+                int len;
+                bool flag = false;
+                for (len = 2; len <= n - pos; len++) {
+                    flag = true;
+                    int l = pos, r = min(pos + len - 1, n - 1);
+                    while (l <= r)
                     {
-                        s.pop();
-                        tmp++;
+                        if (s[l] == s[r]) {
+                            l++;
+                            r--;
+                        }
+                        else {
+                            flag = false;
+                            break;
+                        }
                     }
-                    else
-                    {
+                    if (flag) {
+                        pos += len;
+                        operNum++;
                         break;
                     }
                 }
-                if (s.empty())
-                    break;
-            }
-            if (s.empty())
-                reflag = tmp;
-
-            // palindrome
-            int len;
-            for (len = 2; len <= n - ll; len++)
-            {
-                bool flag = true;
-                int l = ll, r = ll + len - 1;
-                while (l <= r)
-                {
-                    if (str[l] == str[r])
-                    {
-                        l++;
-                        r--;
-                    }
-                    else
-                    {
-                        flag = false;
-                        break;
-                    }
-                }
-                if (flag)
-                {
-                    paflag = len;
+                if (len >= n - pos - 1 && !flag) {
                     break;
                 }
-            }
-
-            // all
-            if (reflag || paflag)
-            {
-                if (reflag == 0)
-                {
-                    ans++;
-                    ll += paflag;
-                    continue;
-                }
-                else if (paflag == 0)
-                {
-                    ans++;
-                    ll += reflag;
-                    continue;
-                }
-                else if (reflag <= paflag)
-                {
-                    ans++;
-                    ll += reflag;
-                    continue;
-                }
-                else
-                {
-                    ans++;
-                    ll += paflag;
-                    continue;
-                }
-            }
-            else
-            {
-                break;
             }
         }
-
-        cout << ans << " " << n - ll << '\n';
+        cout << operNum << " " << n - pos << '\n';
     }
 
     return 0;
 }
+
+// 不找规律的纯模拟 pertest passed but TLE at final test 10
+
+// int length;
+// int n, ans = 0;
+
+// int main()
+// {
+//     ios_base::sync_with_stdio(false);
+//     cin.tie(0);
+//     cout.tie(0);
+//     int t;
+//     cin >> t;
+//     while (t--)
+//     {
+//         cin >> n;
+//         string str;
+//         cin >> str;
+//         ans = 0, length = n;
+//         int ll = 0;
+//         while (ll <= n)
+//         {
+//             int reflag = 0, paflag = 0;
+//             // regular
+//             stack<char> s;
+//             int tmp = 0;
+//             for (int i = ll; i < n; i++)
+//             {
+//                 if (str[i] == '(')
+//                 {
+//                     s.push('(');
+//                     tmp++;
+//                 }
+//                 else
+//                 {
+//                     if (!s.empty())
+//                     {
+//                         s.pop();
+//                         tmp++;
+//                     }
+//                     else
+//                     {
+//                         break;
+//                     }
+//                 }
+//                 if (s.empty())
+//                     break;
+//             }
+//             if (s.empty())
+//                 reflag = tmp;
+
+//             // palindrome
+//             int len;
+//             for (len = 2; len <= n - ll; len++)
+//             {
+//                 bool flag = true;
+//                 int l = ll, r = ll + len - 1;
+//                 while (l <= r)
+//                 {
+//                     if (str[l] == str[r])
+//                     {
+//                         l++;
+//                         r--;
+//                     }
+//                     else
+//                     {
+//                         flag = false;
+//                         break;
+//                     }
+//                 }
+//                 if (flag)
+//                 {
+//                     paflag = len;
+//                     break;
+//                 }
+//             }
+
+//             // all
+//             if (reflag || paflag)
+//             {
+//                 if (reflag == 0)
+//                 {
+//                     ans++;
+//                     ll += paflag;
+//                     continue;
+//                 }
+//                 else if (paflag == 0)
+//                 {
+//                     ans++;
+//                     ll += reflag;
+//                     continue;
+//                 }
+//                 else if (reflag <= paflag)
+//                 {
+//                     ans++;
+//                     ll += reflag;
+//                     continue;
+//                 }
+//                 else
+//                 {
+//                     ans++;
+//                     ll += paflag;
+//                     continue;
+//                 }
+//             }
+//             else
+//             {
+//                 break;
+//             }
+//         }
+
+//         cout << ans << " " << n - ll << '\n';
+//     }
+
+//     return 0;
+// }
