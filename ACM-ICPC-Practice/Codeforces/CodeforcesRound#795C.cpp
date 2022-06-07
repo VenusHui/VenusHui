b41f7bfa61f6daddc1c8209d428d6b1d47e49548
VenusHui <2913727619@qq.com>
2022-06-07 09:39:32 +0800

    Update CFRound#795(Div.2)

--- ACM-ICPC-Practice/Codeforces/CodeforcesRound#795C.cpp
+++ ACM-ICPC-Practice/Codeforces/CodeforcesRound#795C.cpp
@@ -1,9 +1,9 @@
 /**
- * @platform: AtCoder
- * @problem: AtCodeBeginnerContest253E 
+ * @platform: Codeforces
+ * @problem: CodeforcesRound#795C 
  * @version: Contest Version
  * @author: VenusHui
- * @date: 2022-05-28
+ * @date: 2022-05-31
  */
 #include <bits/stdc++.h>
 typedef long long ll;
@@ -13,11 +13,13 @@ int main() {
     ios_base::sync_with_stdio(false);
     cin.tie(nullptr);
     cout.tie(nullptr);
-    const int mod = 998244353;
-    int n, m, k;
-    cin >> n >> m >> k;
-    ll ans = 0;
-    for (int i = 0; i < n; i++) {
+    int t;
+    cin >> t;
+    while (t--) {
+        int n ,k;
+        cin >> n >> k;
+        string s;
+        cin >> s;
         
     }
 
