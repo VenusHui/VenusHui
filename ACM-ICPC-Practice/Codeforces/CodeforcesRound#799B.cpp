84d13263a4f9d312c41f56bedd84380ad119fe71
VenusHui <2913727619@qq.com>
2022-09-14 23:09:40 +0800

    Update CF Problems

--- ACM-ICPC-Practice/Codeforces/CodeforcesRound#799B.cpp
+++ ACM-ICPC-Practice/Codeforces/CodeforcesRound#799B.cpp
@@ -13,7 +13,27 @@ int main() {
     ios_base::sync_with_stdio(false);
     cin.tie(nullptr);
     cout.tie(nullptr);
-    
+    int t;
+    cin >> t;
+    while (t--) {
+        int n;
+        cin >> n;
+        vector<int> a(n);
+        for (int i = 0; i < n; i++) {
+            cin >> a[i];
+        }
+        unordered_map<int, int> mp;
+        for (int i = 0; i < n; i++) {
+            mp[a[i]]++;
+        }
+        int num = 0;
+        for (auto& it : mp) {
+            if (it.second >= 1) {
+                num += it.second - 1;
+            }
+        }
+        cout << n - (num + 1) / 2 * 2 << '\n';
+    }
 
     return 0;
 }
