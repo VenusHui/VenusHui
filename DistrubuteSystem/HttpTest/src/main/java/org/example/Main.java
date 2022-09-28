c1c5739f716a712d1caf8761273eed08da314b6f
VenusHui <2913727619@qq.com>
2022-09-28 21:02:16 +0800

    Update DistributeSystem Homework2 HttpClient

--- DistrubuteSystem/HttpTest/src/main/java/org/example/Main.java
+++ DistrubuteSystem/HttpTest/src/main/java/org/example/Main.java
@@ -1,6 +1,4 @@
-package org.example;
-
-public class Main {
+package org.example;public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
     }
