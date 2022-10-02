4165d38e2b1c0b416671f48d8754889a1375cbd0
VenusHui <2913727619@qq.com>
2022-10-02 17:48:04 +0800

    Update DistributeSystem Homework2

---
+++ ACM-ICPC-Practice/LeetCode/Interview0109.cpp
@@ -0,0 +1,30 @@
+// 面试题 01.09. 字符串轮转
+class Solution {
+public:
+    bool isFlipedString(string s1, string s2) {
+        if (s1.size() != s2.size()) {
+            return false;
+        }
+        else if (s1.size() == 0) {
+            return true;
+        }
+        string s = s2 + s2;
+        int size = s1.size();
+        int index = 0;
+        bool flag = false;
+        for (int i = 0; i < 2 * size; i++) {
+            if (s1[index] == s[i]) {
+                flag = true;
+                index++;
+                if (index == size) {
+                    break;
+                }
+            }
+            else {
+                index = 0;
+                flag = false;
+            }
+        }
+        return index == size && flag;
+    }
+};
---
+++ DistrubuteSystem/homework2/HttpClient/src/main/java/AppRun.java
@@ -0,0 +1,8 @@
+public class AppRun {
+    public AppRun() {
+    }
+
+    public static void main(String[] args) {
+        System.out.println("App Running!");
+    }
+}
