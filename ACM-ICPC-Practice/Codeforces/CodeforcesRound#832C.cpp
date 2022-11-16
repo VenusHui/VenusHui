9e9833f19a84e3a9b631789ba7d1e8eae1ff1a4a
VenusHui <2913727619@qq.com>
2022-11-17 01:09:37 +0800

    Update CFRound#832(Div.2)

---
+++ ACM-ICPC-Practice/Codeforces/CodeforcesRound#832C.cpp
@@ -0,0 +1,40 @@
+/**
+ * @platform: Codeforces
+ * @problem: CodeforcesRound#832C 
+ * @version: Contest Version
+ * @author: VenusHui
+ * @date: 2022-11-04
+ */
+#include <bits/stdc++.h>
+typedef long long ll;
+using namespace std;
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
+    int t;
+    cin >> t;
+    while (t--) {
+        int n;
+        cin >> n;
+        bool ans = true;
+        vector<int> a(n);
+        for (int i = 0; i < n; i++) {
+            cin >> a[i];
+        }
+        for (int i = 0; i < n; i++) {
+            if (a[i] < a[0]) {
+                ans = false;
+            }
+        }
+        if (!ans) {
+            cout << "Alice" << '\n';
+        }
+        else {
+            cout << "Bob" << '\n';
+        }
+    }
+
+    return 0;
+}
---
+++ ACM-ICPC-Practice/Codeforces/CodeforcesRound#832D.cpp
@@ -0,0 +1,23 @@
+/**
+ * @platform: Codeforces
+ * @problem: CodeforcesRound#832D 
+ * @version: Contest Version
+ * @author: VenusHui
+ * @date: 2022-11-04
+ */
+#include <bits/stdc++.h>
+typedef long long ll;
+using namespace std;
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
+    int t;
+    cin >> t;
+    while (t--) {
+        
+    }
+
+    return 0;
+}
