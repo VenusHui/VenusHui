99dbc072d742ad9d948236bb26de885f41e68f8e
VenusHui <2913727619@qq.com>
2023-01-13 01:15:00 +0800

    Update LeetCode

--- ACM-ICPC-Practice/AcWing/tempCodeRunnerFile.cpp
+++
@@ -1,8 +0,0 @@
-vector<int> ans(n + 1);
-    for (int i = 1; i <= n; i++) {
-        ans[i] = ans[i - 1] + pre[i - 1];
-    }
-    for (int i = 1; i <= n; i++) {
-        cout << ans[i] << " ";
-    }
-    cout << '\n';
---
+++ ACM-ICPC-Practice/LeetCode/P2287.cpp
@@ -0,0 +1,18 @@
+// 2287. 重排字符形成目标字符串
+class Solution {
+public:
+    int rearrangeCharacters(string s, string target) {
+        map<char, int> mps, mpt;
+        for (auto& e : s) {
+            mps[e]++;
+        }
+        for (auto& e : target) {
+            mpt[e]++;
+        }
+        int ans = 1 << 30;
+        for (auto& e : mpt) {
+            ans = min(ans, mps[e.first] / e.second);
+        }
+        return ans >= (1 << 30) ? 0 : ans;
+    }
+};
