51a934b81ab88684497bc40404ed3ca1ac019991
VenusHui <2913727619@qq.com>
2023-01-20 01:00:23 +0800

    Update LeetCode

---
+++ ACM-ICPC/LeetCode/P1817.cpp
@@ -0,0 +1,11 @@
+// 1817. 查找用户活跃分钟数
+class Solution {
+public:
+    vector<int> findingUsersActiveMinutes(vector<vector<int>>& logs, int k) {
+        vector<int> ans(k);
+        map<int, set<int>> mp;
+        for (auto& e : logs) mp[e[0]].insert(e[1]);
+        for (auto& e : mp) ans[e.second.size() - 1]++;
+        return ans;
+    }
+};
