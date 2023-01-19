b704cbfad353e708f40f91d903c5a7f90baf33b4
VenusHui <2913727619@qq.com>
2023-01-19 16:07:26 +0800

    Update LeetCode

---
+++ ACM-ICPC-Practice/LeetCode/P9.cpp
@@ -0,0 +1,14 @@
+// 9. 回文数
+class Solution {
+public:
+    bool isPalindrome(int x) {
+        if (x < 0) return false;
+        string s, t;
+        while (x) {
+            s += x % 10 + '0';
+            x /= 10;
+        }
+        t = s, reverse(t.begin(), t.end());
+        return s == t;
+    }
+};
