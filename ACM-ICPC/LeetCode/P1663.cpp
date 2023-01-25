c2df3b5e4f641674cf633eb4fd9abf5b0a111542
VenusHui <2913727619@qq.com>
2023-01-26 00:43:24 +0800

    Update LeetCode

---
+++ ACM-ICPC/LeetCode/P1663.cpp
@@ -0,0 +1,15 @@
+// 1663. 具有给定数值的最小字符串
+class Solution {
+public:
+    string getSmallestString(int n, int k) {
+        k -= n;
+        vector<int> res(n, 'a');
+        for (int i = n - 1; i >= 0 && k > 0; i--) {
+            if (k - 25 >= 0) res[i] += 25, k -= 25;
+            else res[i] += k, k -= k;
+        }
+        string ans;
+        for (int i = 0; i < n; i++) ans += static_cast<char>(res[i]);
+        return ans;
+    }
+};
