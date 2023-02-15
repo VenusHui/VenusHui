cf93f79e9769e08bab1c08b94f4323b234ba9ed8
VenusHui <2913727619@qq.com>
2023-02-15 22:12:23 +0800

    Update Leetcode

---
+++ ACM-ICPC/LeetCode/P1234.cpp
@@ -0,0 +1,21 @@
+// 1234. 替换子串得到平衡字符串
+class Solution {
+public:
+    int balancedString(string s) {
+        int size = s.size(), len = s.size() / 4;
+        map<char, int> mp;
+        for (auto& e : s) mp[e]++;
+        int ans = 0x3f3f3f3f;
+        if (mp['Q'] <= len && mp['W'] <= len && mp['E'] <= len && mp['R'] <= len) return 0;
+        for (int l = 0, r = 0; l < size; l++) {
+            while (r < size && !(mp['Q'] <= len && mp['W'] <= len && mp['E'] <= len && mp['R'] <= len)) {
+                mp[s[r]]--;
+                r++;
+            }
+            if (!(mp['Q'] <= len && mp['W'] <= len && mp['E'] <= len && mp['R'] <= len)) break;
+            ans = min(ans, r - l);
+            mp[s[l]]++;
+        }
+        return ans;
+    }
+};
