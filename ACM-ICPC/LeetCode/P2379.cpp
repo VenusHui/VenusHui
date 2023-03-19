8bafd9734c9b8ed92b618feeb73dbc72923bd0b0
VenusHui <2913727619@qq.com>
2023-03-19 17:59:15 +0800

    Update Leetcode

---
+++ ACM-ICPC/LeetCode/P2379.cpp
@@ -0,0 +1,17 @@
+// 2379. 得到 K 个黑块的最少涂色次数
+class Solution {
+public:
+    int minimumRecolors(string blocks, int k) {
+        int ans = 0x3f3f3f3f, size = blocks.size();
+        for (int i = 0; i < size; i++) {
+            int tmp = 0, num = 0;
+            for (int j = i; j < size; j++) {
+                if (blocks[j] == 'B') num++;
+                else tmp++, num++;
+                if (num == k) break;
+            }
+            if (num == k) ans = min(ans, tmp);
+        }
+        return ans;
+    }
+};
