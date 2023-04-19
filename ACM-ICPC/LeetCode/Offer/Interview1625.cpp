53bd858c6a23969f5acea9700caf79437fc98d5c
VenusHui <2913727619@qq.com>
2023-04-19 21:30:49 +0800

    Update Leetcode

---
+++ ACM-ICPC/LeetCode/Offer/Interview1625.cpp
@@ -0,0 +1,71 @@
+// 面试题 16.25. LRU 缓存
+class LRUCache {
+private:
+    struct DLinkedNode {
+        int key, value;
+        DLinkedNode* prev;
+        DLinkedNode* next;
+        DLinkedNode(): key(0), value(0), prev(nullptr), next(nullptr) {}
+        DLinkedNode(int _key, int _value): key(_key), value(_value), prev(nullptr), next(nullptr) {}
+    };
+    int size, capacity;
+    DLinkedNode* head;
+    DLinkedNode* tail;
+    unordered_map<int, DLinkedNode*> mp;
+    inline void addToHead(DLinkedNode* node) {
+        node->next = head->next;
+        node->prev = head;
+        head->next->prev = node;
+        head->next = node;
+    }
+    inline void removeNode(DLinkedNode* node) {
+        node->prev->next = node->next;
+        node->next->prev = node->prev;
+    }
+public:
+    LRUCache(int capacity) {
+        size = 0;
+        this->capacity = capacity;
+        head = new DLinkedNode();
+        tail = new DLinkedNode();
+        head->next = tail;
+        tail->prev = head;
+    }
+    
+    int get(int key) {
+        if (!mp.count(key)) return -1;
+        DLinkedNode* node = mp[key];
+        removeNode(node);
+        addToHead(node);
+        return node->value;
+    }
+    
+    void put(int key, int value) {
+        if (mp.count(key)) {
+            DLinkedNode* node = mp[key];
+            node->value = value;
+            removeNode(node);
+            addToHead(node);
+        }
+        else {
+            DLinkedNode* node = new DLinkedNode(key, value);
+            mp[key] = node;
+            addToHead(node);
+            size++;
+            if (size > capacity) {
+                DLinkedNode* reNode = tail->prev;
+                mp.erase(reNode->key);
+                removeNode(reNode);
+                delete reNode;
+                size--;
+            }
+        }
+    }
+};
+
+/**
+ * Your LRUCache object will be instantiated and called as such:
+ * LRUCache* obj = new LRUCache(capacity);
+ * int param_1 = obj->get(key);
+ * obj->put(key,value);
+ */
