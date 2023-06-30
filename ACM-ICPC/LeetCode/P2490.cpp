1cd534423e316bbbeb357f039b635c4564d79afc
VenusHui <2913727619@qq.com>
2023-06-30 12:42:01 +0800

    Update Leetcode

--- ACM-ICPC/LeetCode/P2490.cpp
+++ ACM-ICPC/LeetCode/P2490.cpp
@@ -25,3 +25,32 @@ public:
         return flag;
     }
 };
+
+
+class Solution {
+public:
+    bool isCircularSentence(string sentence) {
+        vector<string> words;
+        int pos = 0;
+        string s(sentence);
+        while (pos < s.size()) {
+            while (pos < s.size() && s[pos] == ' ') pos++;
+            int start = pos;
+            while (pos < s.size() && s[pos] != ' ') pos++;
+            if (pos > start) words.push_back(s.substr(start, pos - start));
+        }
+        bool ans = true;
+        int size = words.size();
+        for (int i = 0; i < size; i++) {
+            if (i != 0 && words[i][0] != words[i - 1][words[i - 1].size() - 1]) {
+                ans = false;
+                break;
+            }
+            if (i == size - 1 && words[i][words[i].size() - 1] != words[0][0]) {
+                ans = false;
+                break;
+            }
+        }
+        return ans;
+    }
+};
