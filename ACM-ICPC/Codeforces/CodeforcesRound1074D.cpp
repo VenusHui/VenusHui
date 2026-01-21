e39470f452f356a92937b422af909d646a4ac7a7
VenusHui <2913727619@qq.com>
2026-01-21 08:21:02 +0800

    CodeforcesRound 1074 D

--- ACM-ICPC/Codeforces/CodeforcesRound1074D.cpp
+++ ACM-ICPC/Codeforces/CodeforcesRound1074D.cpp
@@ -1,65 +1,51 @@
 /**
  * @platform: Codeforces
- * @problem: CodeforcesRound1074D 
+ * @problem: CodeforcesRound1074D
  * @version: Contest Version
  * @author: VenusHui
  * @date: 2026-01-18
  */
 #include <bits/stdc++.h>
+#include <iostream>
 typedef long long ll;
 using namespace std;
 
 int main() {
-    ios_base::sync_with_stdio(false);
-    cin.tie(nullptr);
-    cout.tie(nullptr);
-    int t;
-    cin >> t;
-    while (t--) {
-        int n, m, h;
-        cin >> n >> m >> h;
-        vector<int> a(n);
-        for (int i = 0; i < n; i++) cin >> a[i];
-        vector<int> diff(n, 0);
-        vector<int> base_version(n, 0);
-        vector<int> current_value(n);
-        int global_version = 0;
-        bool need_reset = false;
-        for (int i = 0; i < n; i++) {
-            current_value[i] = a[i];
-        }
-        for (int i = 0; i < m; i++) {
-            int b, c;
-            cin >> b >> c;
-            b--;
-            if (need_reset) {
-                global_version++;
-                need_reset = false;
-                diff[b] = c;
-                base_version[b] = global_version;
-            } else {
-                if (base_version[b] < global_version) {
-                    diff[b] = c;
-                    base_version[b] = global_version;
-                } else {
-                    diff[b] += c;
-                }
-            }
-            int current_base = (base_version[b] == global_version) ? a[b] : current_value[b];
-            if (current_base + diff[b] > h) {
-                need_reset = true;
-            } else {
-                current_value[b] = current_base + diff[b];
-            }
-        }
-        for (int j = 0; j < n; j++) {
-            if (base_version[j] == global_version && !need_reset) {
-                cout << a[j] + diff[j] << " ";
-            } else {
-                cout << a[j] << " ";
-            }
-        }
-        cout << '\n';
+  ios_base::sync_with_stdio(false);
+  cin.tie(nullptr);
+  cout.tie(nullptr);
+  int t;
+  cin >> t;
+  while (t--) {
+    int n, m, h;
+    cin >> n >> m >> h;
+    vector<int> a(n);
+    for (int i = 0; i < n; i++)
+      cin >> a[i];
+    vector<int> ac = a;
+    vector<int> v(n, -1);
+    int last_reset = -1;
+    for (int i = 0; i < m; i++) {
+      int b, c;
+      cin >> b >> c;
+      b--;
+      if (v[b] < last_reset) {
+        ac[b] = a[b];
+      }
+      ac[b] += c;
+      if (ac[b] > h) {
+        last_reset = i;
+        ac[b] = a[b];
+      }
+      v[b] = i;
     }
-    return 0;
+    for (int i = 0; i < n; i++) {
+      if (v[i] < last_reset)
+        cout << a[i] << " ";
+      else
+        cout << ac[i] << " ";
+    }
+    cout << '\n';
+  }
+  return 0;
 }
